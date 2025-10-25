2f6a7a83286f5779b80ef97324f9c0a59027c4d0
Lee Sruim <sssurim@naver.com>
2025-10-26 00:23:44 +0900

    feat:  위치 업데이트

---
+++ src/main/java/racingcar/Model/Position.java
@@ -0,0 +1,15 @@
+package racingcar.Model;
+
+public class Position {
+    private int value = 0;
+
+    public void increase() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+//위치 업데이트
